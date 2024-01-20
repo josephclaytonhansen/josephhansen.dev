f6c21539aa43a43c66a41e0a6cd5099b17da74bf
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-01-20 11:26:06 -0600

    matomo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue"
 import "./style.css"
 import App from "./App.vue"
+import VueMatomo from "vue-matomo"
 
 import Main from "./components/main/Main.vue"
 import {
@@ -21,6 +22,16 @@ const router = createRouter({
 
 const app = createApp(App)
 
+app.use(VueMatomo, {
+  host: "https://josephhansen.dev",
+  siteId: 1,
+  router: router,
+  enableLinkTracking: true,
+  disableCookies: true,
+})
+
 app.use(router)
 
 app.mount("#app")
+
+window._paq.push(["trackPageView"])
