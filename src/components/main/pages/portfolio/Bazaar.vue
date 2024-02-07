e89e2c195632c1ec2d63b298d76fbae3400d5108
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-02-06 20:10:29 -0600

    buildbot: 2/6 20:10

--- src/components/main/pages/portfolio/Bazaar.vue
+++ src/components/main/pages/portfolio/Bazaar.vue
@@ -23,16 +23,10 @@
     "Bazaar product listing",
   ])
 
-  const pClass = (brightness) => {
-    if (brightness >= 4) {
-      return "text-slate-800"
-    } else if (brightness == 3) {
-      return "text-slate-200"
-    } else if (brightness == 2) {
-      return "text-slate-300"
-    } else if (brightness == 1) {
-      return "text-slate-300"
-    }
+  const images = {
+    planning: "https://images.josephhansen.dev/uploads/file2024-02-0619-1707270927524.webp",
+    figma: "https://images.josephhansen.dev/uploads/file2024-02-0619-1707271001166.webp",
+
   }
 </script>
 
@@ -47,17 +41,22 @@
       <h3 class="text-2xl font-bold text-inherit">
         The vision: a one-stop shop for Blender users
       </h3>
-      <p>Lorem ipsum</p>
+      <p class="text-inherit">
+        When Bart from BlenderNation approached me with the idea for Bazaar, I was pumped. Nothing quite like Bazaar existed at the time- one central hub for Blender users to find tutorials, resources, assets, and add-ons. I was heavily involved with every step of the process of making the Bazaar come to life, and the end result is fantastic.
+      </p>
+      <figure><img :src = "images.planning" class="rounded-xl"><figcaption>Bazaar's planning board</figcaption></figure>
+      <p class="text-inherit">
+        With the above Figma document as a guide from Bart, I dove into both design and figuring out the backend details for managing the complex data the site would be handling. Bart wanted to do this through WordPress, and I was able to use my WordPress expertise to recommend AdvancedCustomFields to do a lot of the major data-wrangling. I also built the theme from scratch, to make sure it was as simplifie and lightweight as possible while still providing beautiful, responsive, and functional results.
+        <figure><img :src = "images.figma"><figcaption>My approved design for the Bazaar</figcaption></figure>
+      </p>
       <h3 class="text-2xl font-bold text-inherit">
         Tight deadlines and high stakes
       </h3>
-      <p>Lorem ipsum</p>
-      <h3 class="text-2xl font-bold text-inherit">From concept to results</h3>
-      <p>Lorem ipsum</p>
+      <p>When Bart approached me, there was about a month until the next Blender Conference, a massive community event that he hoped to present Bazaar at. At this point, the Bazaar was just an idea- there wasn't even a logo yet. Long story short- Bazaar launched successfully with time to spare. I believe this project showed  I can work well under pressure and with tight deadlines to achieve exactly what a client needs.</p>
       <h3 class="text-2xl font-bold text-inherit">
         Security- keeping the Bazaar safe
       </h3>
-      <p>Lorem ipsum</p>
+      <p>Among the other development, I've implemented powerful security guardrails across Bazaar, ranging from DDoS protection to a comprehensive firewall. I'm proud to report that my security measures are currently preventing around 4500 attacks a month, with that number growing larger all the time.</p>
     </slot></sliderAndGallery
   >
 </template>
